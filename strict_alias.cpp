76f9a7b9ef9dd71ab5442166008c650644b1ccfb
Obsidian-fun <obsidiancrystal@tutanota.com>
2025-01-01 03:53:33 +0530

    new file:   strict_alias.cpp

---
+++ strict_alias.cpp
@@ -0,0 +1,18 @@
+/***
+converting a double * to an uint64_t *,
+page 94
+
+***/
+
+#include "cstdio"
+#include "cstdint"
+
+int main() {
+	double x = 100;
+	const auto orig_x = x;
+	auto x_as_ui = (unint64_t *) (&x);
+	*x_as_ui |= 0x8000000000000000;
+	printf("orig_x: %0.2f x: %0.2f &x: %p, &x_as_ui: %p\n", orig_x, x, &x, &x_as_ui);
+
+}
+
